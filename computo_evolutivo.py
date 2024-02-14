c2199f4545a931f9cfe2de0ff3f30530041f25ef
jesusmartinrosas <jesusmartin-25@ciencias.unam.mx>
2024-02-13 21:46:08 -0600

    Funciones con dimesion y funcionalidad de terminal

--- computo_evolutivo.py
+++ computo_evolutivo.py
@@ -1,36 +1,67 @@
 # Rastringin 
 
+# Imports 
 import math
+import sys
 
-def rastringin(x: list):
-    """
-    Rastringin con x lista
-    :param x: lista de valores
-    """
+# Funcion Rastringin
+def rastringin(x: list, dimension: int):
     suma = 0
-    for i in range(len(x)):
+    for i in range(dimension):
         suma += x[i] ** 2 - 10 * math.cos(2 * math.pi * x[i])
-    return 10 * len(x) + suma
+    return 10 * dimension + suma
 
-def rosembrock(x: list):
-    """
-    Rosembrock con x lista
-    :param x: lista de valores
-    """
+# Funcion Rosenbrock
+def rosembrock(x: list, dimension: int):
     suma = 0
-    for i in range(len(x) - 1):
+    for i in range(dimension - 1):
         suma += 100 * (x[i + 1] - x[i] ** 2) ** 2 + (x[i] - 1) ** 2
     return suma
 
-
-def griewank(x: list):
-    """
-    Griewank con x lista
-    :param x: lista de valores
-    """
+# Funcion Schwefel
+def schwefel(x: list, dimension: int):
     suma = 0
-    producto = 1
-    for i in range(len(x)):
-        suma += x[i] ** 2 / 4000
-        producto *= math.cos(x[i] / math.sqrt(i + 1))
-    return suma - producto + 1
+    for i in range(dimension):
+        suma += x[i] * math.sin(math.sqrt(abs(x[i])))
+    return 418.9829 * dimension - suma
+
+# Evaluar funcion
+def evaluar(funcion: str, dimension: int, *args):
+    """
+    Evalua una funcion en un punto dado y
+    dependiendo de la funcion que se le pase
+    evalua la funcion correspondiente con los
+    argumentos dados y su dimension.
+    """
+
+    if funcion == "rastringin":
+        print(rastringin(list(args), dimension)) 
+    elif funcion == "rosembrock":
+        print(rosembrock(list(args), dimension))
+    elif funcion == "schwefel":
+        print(schwefel(list(args), dimension))
+    else:
+        print("Funcion no encontrada")
+
+
+
+if __name__ == "__main__":
+# Se espera que se pase la funcion, la dimension y los argumentos
+    if len(sys.argv) < 4: # Si no se pasan los argumentos necesarios se termina el programa
+        print("Faltan argumentos")
+        sys.exit(1)
+
+    # Se obtienen los argumentos
+    funcion = sys.argv[1]
+    dimension = int(sys.argv[2])
+    args = [float(x) for x in sys.argv[3:]]
+
+    # Se evalua la funcion
+    resultado = evaluar(funcion, dimension, *args)
+    # Se imprime el resultado en terminal
+    print(resultado)
+    
+
+
+
+

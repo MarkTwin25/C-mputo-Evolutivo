60b935673a9d4732f9c1b87d6e70a57602d597c2
carlos-crea <carlos.ed0205@ciencias.unam.mx>
2024-02-28 17:51:30 -0600

    Finalizado

--- Tarea2/Ejercicio1.py
+++ Tarea2/Ejercicio1.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-########################## Ejercicio 2 ########################## 
-
 # Función que recibe un número natural y lo convierte a su representación binaria
 def binario_a_decimal(arreglo):
     """
@@ -127,3 +125,46 @@ def decodificar_vector(x_cod, num_bits, a, b):
     # Usamos la funcion decodificar implementada anteriormente
     x = [decodificar(i, num_bits, a, b) for i in x_cod]
     return x
+
+# Ejemplo binario_a_decimal
+binario_ejemplo = [1, 0, 1, 0] 
+decimal_resultado = binario_a_decimal(binario_ejemplo)
+print(f"Ejemplo binario_a_decimal: {binario_ejemplo} -> Decimal: {decimal_resultado}")
+
+# Ejemplo decimal_a_binario
+decimal_ejemplo = 10
+num_bits_ejemplo = 4
+binario_resultado = decimal_a_binario(decimal_ejemplo, num_bits_ejemplo)
+print(f"Ejemplo decimal_a_binario: Decimal {decimal_ejemplo} -> Binario: {binario_resultado}")
+
+# Ejemplo codificar
+x_ejemplo = 0.75
+num_bits_codificar = 4
+a_codificar = 0.0
+b_codificar = 1.0
+binario_codificado = codificar(x_ejemplo, num_bits_codificar, a_codificar, b_codificar)
+print(f"Ejemplo codificar: Real {x_ejemplo} -> Binario codificado: {binario_codificado}")
+
+# Ejemplo decodificar
+binario_decodificar = [1, 0, 0, 1]
+num_bits_decodificar = 4
+a_decodificar = 0.0
+b_decodificar = 1.0
+x_decodificado = decodificar(binario_decodificar, num_bits_decodificar, a_decodificar, b_decodificar)
+print(f"Ejemplo decodificar: Binario {binario_decodificar} -> Real decodificado: {x_decodificado}")
+
+# Ejemplo codificar_vector
+vector_ejemplo = [0.2, 0.5, 0.8]
+num_bits_codificar_vector = 4
+a_codificar_vector = 0.0
+b_codificar_vector = 1.0
+vector_codificado = codificar_vector(vector_ejemplo, num_bits_codificar_vector, a_codificar_vector, b_codificar_vector)
+print(f"Ejemplo codificar_vector: Vector {vector_ejemplo} -> Vector codificado: {vector_codificado}")
+
+# Ejemplo decodificar_vector
+vector_binario_decodificar = [[1, 0, 0, 1], [0, 1, 0, 1], [1, 1, 1, 1]]
+num_bits_decodificar_vector = 4
+a_decodificar_vector = 0.0
+b_decodificar_vector = 1.0
+vector_decodificado = decodificar_vector(vector_binario_decodificar, num_bits_decodificar_vector, a_decodificar_vector, b_decodificar_vector)
+print(f"Ejemplo decodificar_vector: Vector binario {vector_binario_decodificar} -> Vector decodificado: {vector_decodificado}")
